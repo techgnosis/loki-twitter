942e95a806f3efb92e40a50ca9dcb3ad1a9ba180
James Musselwhite <james.musselwhite@grafana.com>
2021-11-14 10:26:05 -0800

    streaming tweets is working

---
+++ stream.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import json
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+
+def create_url():
+    return "https://api.twitter.com/2/tweets/sample/stream?tweet.fields=created_at"
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2SampledStreamPython"
+    return r
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth, stream=True)
+    print(response.status_code)
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+
+
+def main():
+    url = create_url()
+    timeout = 0
+    while True:
+        connect_to_endpoint(url)
+        timeout += 1
+
+
+if __name__ == "__main__":
+    main()
